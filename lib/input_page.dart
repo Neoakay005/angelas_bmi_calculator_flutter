993e49f3fe0ed4501b0aff92aa2c192c026553b0
Neoakay005 <akayhalil0505@gmail.com>
2025-11-17 17:38:20 +0300

    Add Font Awesome icons to InputPage and update ReusableCard implementation

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // Constants
 const buttomContainerHeight = 80.0;
@@ -22,8 +23,24 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableCard(colours: activeCardColour)),
-                Expanded(child: ReusableCard(colours: activeCardColour)),
+                Expanded(
+                  child: ReusableCard(
+                    colours: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      gender: "Male",
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colours: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      gender: "Female",
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -48,10 +65,30 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent({required this.icon, required this.gender});
+
+  final IconData icon;
+  final String gender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80),
+        Padding(padding: EdgeInsets.only(bottom: 15)),
+        Text(gender, style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98))),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({required this.colours});
+  const ReusableCard({required this.colours, this.cardChild});
 
   final Color colours;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +98,7 @@ class ReusableCard extends StatelessWidget {
         color: colours,
         borderRadius: BorderRadius.circular(15),
       ),
+      child: cardChild,
     );
   }
 }
